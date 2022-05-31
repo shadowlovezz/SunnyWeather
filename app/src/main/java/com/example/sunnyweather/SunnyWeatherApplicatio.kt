c10a645fb60ce9cb84c5dff355a32915fe5c6f3b
shadowlovezz <809352504@qq.com>
2022-06-01 01:02:48 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/example/sunnyweather/SunnyWeatherApplicatio.kt
@@ -0,0 +1,17 @@
+package com.example.sunnyweather
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+        const val TOKEN = "Bgq7zoPwaaSdTfZN"
+    }
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
